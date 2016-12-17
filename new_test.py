ce7b55a180b8e2bfc1c1ce621936f8a302115d25
LukieJoe <LukePaltzer@gmail.com>
2016-12-16 23:12:57 -0600

    cmd and comment testing

---
+++ new_test.py
@@ -0,0 +1,17 @@
+'''
+added this file from cmd line
+
+del /f to delete cmd
+rm to delete linux
+
+touch to add file linux
+echo.> to add file cmd
+
+MOVE to move cmd
+mv to move linux
+
+notepad to edit cmd
+nano to edit linux
+'''
+
+print("cmd line help")
